b297fe9d32b83ecc68384e9e7789c672e10ade35
Maxim Lippeveld <maxim.lippeveld@imdc.be>
2024-01-15 11:08:29 +0100

    Set feature dir

--- src/docker-in-docker/install.sh
+++ src/docker-in-docker/install.sh
@@ -19,6 +19,8 @@ MICROSOFT_GPG_KEYS_URI="https://packages.microsoft.com/keys/microsoft.asc"
 DOCKER_MOBY_ARCHIVE_VERSION_CODENAMES="bookworm buster bullseye bionic focal jammy"
 DOCKER_LICENSED_ARCHIVE_VERSION_CODENAMES="bookworm buster bullseye bionic focal hirsute impish jammy"
 
+FEATURE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Default: Exit on any failure.
 set -e
 
